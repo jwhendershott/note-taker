f2619150387cb2e1879c403ec220ea5957d0d750
Jacob Hendershott <jwhendershott@gmail.com>
2021-01-31 18:22:23 -0600

    updated server.js

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.urlencoded({estrended: true}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static('public'));
 
-app.get('/',(req, res) => res.sendFile(path.join(__dirname, 'Develop/public/index.html')));
-app.get('/notes',(req, res) => res.sendFile(path.join(__dirname, 'Develop/public/notes.html')));
-app.get('/api/notes', function(req,res) {
-    return res.sendFile(path.json(__dirname, 'Develop/db/db.json'));
-})
-
+app.get('/',(req, res) => res.sendFile(path.join(__dirname, './Develop/public/index.html')));
+app.get('/notes',(req, res) => res.sendFile(path.join(__dirname, './Develop/public/notes.html')));
+app.get('/api/notes', (req,res) => res.sendFile(path.json()));
 
 
 app.listen(PORT, () => console.log(`App listiening on http://localhost:${PORT}`));
