3b47642bad5a478ac32d18865dc35e472650d6a2
Jacob Hendershott <jwhendershott@gmail.com>
2021-01-31 22:57:13 -0600

    updated routes

--- server.js
+++ server.js
@@ -9,9 +9,13 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/',(req, res) => res.sendFile(path.join(__dirname, './Develop/public/index.html')));
-app.get('/notes',(req, res) => res.sendFile(path.join(__dirname, './Develop/public/notes.html')));
-app.get('/api/notes', (req,res) => res.sendFile(path.json()));
+app.get('/notes',(req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+app.get('/',(req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+app.get("/api/notes", (req, res) => res.sendFile(path.join(__dirname, "/db/db.json")));
+
+// app.post('/api/notes', (req,res) => {
+
+// })
 
 
 app.listen(PORT, () => console.log(`App listiening on http://localhost:${PORT}`));
